0c5f625aa8614b93ac797f04ad5e26bf545102e7
fbt0wizard <oladipomustapha001@gmail.com>
2021-09-05 10:53:15 -0400

    query

--- javascript/test-page.js
+++ javascript/test-page.js
@@ -4,6 +4,18 @@ const answered = [];
 
 const correct = [];
 
+var randomArray = [];
+
+// Assigning numbers to question
+
+var qOne = document.getElementById("question1");
+var qTwo = document.getElementById("question2");
+var qThree = document.getElementById("question3");
+var qFour = document.getElementById("question4");
+var qFive = document.getElementById("question5");
+
+const questions = [qOne, qTwo, qThree, qFour, qFive];
+
 // count down time
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
@@ -36,27 +48,51 @@ function stop() {
 }
 
 function start() {
-    randomNumber();
+    var result = questions[Math.floor(Math.random()*questions.length)];
+    randomArray.push(result);
+    answered.push(result);
     var hot = document.getElementById("startPage");
     if (hot.style.display = "block") {
         $(hot).fadeOut();
-        $("#question1").fadeIn(100);
-        $("#backNext").fadeIn(100);
-        $("#finish").fadeIn(100);
+        $(result).fadeIn();
+        $("#backNext").fadeIn();
+        $("#finish").fadeIn();
     };
     countDown();
-
-    console.log(ranNums);
 };
 
-function randomNumber() {
-        var nums = [1,2,3,4,5];
-        i = nums.length,
-        j = 0;
+function next() {
+    console.log(answered.length);
+    if(answered.length == 5) {
+        alert("Test Completed");
+        return;
+    }
+    var result = questions[Math.floor(Math.random()*questions.length)];
+    if(answered.includes(result)) {
+        next();
+        return;
+    }
+    if(answered.length == 1) {
+        var oneQ = answered.slice(-1);
+        $(oneQ).fadeOut();
+        $(result).fadeIn();
+    }
+    if(answered.length > 1) {
+    var lastQ = answered.slice(-1);
+    $(lastQ).fadeOut();
+    $(result).fadeIn();
+    }
+    answered.push(result);
 
-        while (i--) {
-            j = Math.floor(Math.random() * (i+1));
-            ranNums.push(nums[j]);
-            nums.splice(j,1);
-        }
 }
+
+
+// function nextQ() {
+//     var result = questions[Math.floor(Math.random()*questions.length)];
+
+//     if(randomArray.includes(randomGen)) {
+//         nextQ();
+//     } else {
+//        randomArray.push(randomGen);
+//     }
+// }
--- test.js
+++ test.js
@@ -1,26 +1,73 @@
-var nums = [1, 2, 3, 4, 5],
-    ranNums = [],
-    i = nums.length,
-    j = 0;
 
-while (i--) {
-    j = Math.floor(Math.random() * (i+1));
-    ranNums.push(nums[j]);
-    nums.splice(j,1);
-}
+// var qOne = "who you?";
+// var qTwo = "who me?";
+// var qThree = "who they?";
+// var qFour = "who them?";
+// var qFive = 455;
 
-console.log(ranNums);
+// var q1 = qOne;
+// var q2 = qTwo;
+// var q3 = qThree;
+// var q4 = qFour;
+// var q5 = qFive;
 
-var result = ranNums.splice(2);
-console.log(result);
+// function randomNumber() {
+//     var nums = [1,2,3,4,5];
+//     i = nums.length,
+//     j = 0;
 
-var qOne = "who you?";
-var qTwo = "who me?";
-var qThree = "who they?";
-var qFour = "who them?";
-var qFive = "who where?";
+//     while (i--) {
+//         j = Math.floor(Math.random() * (i+1));
+//         ranNums.push(nums[j]);
+//         nums.splice(j,1);
+//     }
+//     console.log(ranNums);
+// }
 
 
-const question = [qOne, qTwo, qThree, qFour, qFive];
+// var randomArray = [];
 
-// document.getElementById("check").innerHTML = question;
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
+
+// function run() {
+//     let randomGen = getRandomInt(6);
+//     console.log(randomGen);
+//       if(randomArray.includes(randomGen)) {
+//       run();
+//     } else {
+//      randomArray.push(randomGen);
+//     }
+
+//  document.getElementById("check").innerHTML = randomGen;
+//  document.getElementById("result").innerHTML = randomArray;
+// };
+
+
+// console.log(result);
+// console.log(typeof result);
+
+
+
+// var hop = document.getElementsByName("q1");
+// hop.addEventListener('click', function() {
+//   pow();
+// }, false
+// );
+
+
+
+// function pow() {
+//   alert("i worked");
+// }
+
+var q1 = document.getElementById("check");
+var q2 = document.getElementById("a1A");
+var q3 = document.getElementById("btn");
+
+const test = [q1, q2, q3];
+console.log(test.length);
+console.log(test.slice(-1));
+// console.log(typeof cut);
+// console.log(test.length);
